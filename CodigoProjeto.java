673ae08e7a669115946842e0a1c144fe3fd633a3
JoaoTomaas <revealconcepts@gmail.com>
2018-11-22 21:06:49 +0000

    Update CodigoProjeto.java

--- CodigoProjeto.java
+++ CodigoProjeto.java
@@ -5,118 +5,118 @@ import java.sql.*;
 
 class Metodos {
 
-    void executa_login (String user, String pass, Connection connection) throws SQLException {
+        void executa_login (String user, String pass, Connection connection) throws SQLException {
 
-        String SQL = "select username, password from utilizador where username = ?";
-        PreparedStatement pstmt = connection.prepareStatement(SQL);
-        pstmt.setString(1, user);
-        ResultSet rs = pstmt.executeQuery();
+            String SQL = "select username, password from utilizador where username = ?";
+            PreparedStatement pstmt = connection.prepareStatement(SQL);
+            pstmt.setString(1, user);
+            ResultSet rs = pstmt.executeQuery();
 
-        if (rs.next()) {
-            System.out.println("Esse mano ja existe na base de dados");
+            if (rs.next()) {
+                System.out.println("Esse mano ja existe na base de dados");
 
-            if (pass.equals(rs.getString(2))) {
-                System.out.println("Password correta, welcome to DropMusic");
+                if (pass.equals(rs.getString(2))) {
+                    System.out.println("Password correta, welcome to DropMusic");
+                } else {
+                    System.out.println("Password errada, try again!");
+                }
             } else {
-                System.out.println("Password errada, try again!");
+                System.out.println("Tens que te registar mano");
             }
-        } else {
-            System.out.println("Tens que te registar mano");
+
+            rs.close();
+            pstmt.close();
         }
 
-        rs.close();
-        pstmt.close();
-    }
+        void executa_registo (String user, String pass, Connection connection) throws SQLException {
 
-    void executa_registo (String user, String pass, Connection connection) throws SQLException {
+            String SQL = "select username, password from utilizador where username = ?";
+            PreparedStatement pstmt = connection.prepareStatement(SQL);
+            pstmt.setString(1, user);
+            ResultSet rs = pstmt.executeQuery();
 
-        String SQL = "select username, password from utilizador where username = ?";
-        PreparedStatement pstmt = connection.prepareStatement(SQL);
-        pstmt.setString(1, user);
-        ResultSet rs = pstmt.executeQuery();
+            if (rs.next()){
+                System.out.println("Esse mano ja se encontra registado");
+            }
+            else{
+                String SQLRegister = "insert into utilizador values (username, password) "; //TERMINAR
+                PreparedStatement stmt = connection.prepareStatement(SQLRegister);
+                //stmt.setString(1, user);
+                rs = pstmt.executeQuery(); //Verificar se e executador //Verificar se e necessario inicializar a null o ResultSet
 
-        if (rs.next()){
-            System.out.println("Esse mano ja se encontra registado");
+                System.out.println("Utilizador registado, welcome to DropMusic");
+            }
+
+
         }
-        else{
-            String SQLRegister = "insert into utilizador values (username, password) "; //TERMINAR
-            PreparedStatement stmt = connection.prepareStatement(SQLRegister);
-            //stmt.setString(1, user);
-            rs = pstmt.executeQuery(); //Verificar se e executador //Verificar se e necessario inicializar a null o ResultSet
 
-            System.out.println("Utilizador registado, welcome to DropMusic");
-        }
+        void executa_search_music (String search_type, String word, Connection connection) throws SQLException{
 
+            if (search_type.equals("all")) {
+                String ALLSQL = "SELECT music_name FROM public.musica WHERE id_musica =" +
+                                    "(SELECT id_musica FROM public.musicas_de_album WHERE id_album = " +
+                                        "(SELECT id_album FROM public.album WHERE album_name = ?))\n" +
+                        "UNION\n" +
 
-    }
+                        "SELECT music_name FROM public.musica WHERE music_name = ?\n" +
+                        "UNION\n" +
 
-    void executa_search_music (String search_type, String word, Connection connection) throws SQLException{
+                        "SELECT music_name FROM public.musica WHERE genre = ?\n" +
+                        "UNION\n" +
 
-        if (search_type.equals("all")) {
-            String ALLSQL = "SELECT music_name FROM public.musica WHERE id_musica =" +
-                                "(SELECT id_musica FROM public.musicas_de_album WHERE id_album = " +
-                                    "(SELECT id_album FROM public.album WHERE album_name = ?))\n" +
-                    "UNION\n" +
+                        "SELECT music_name FROM public.musica WHERE id_musica = " +
+                            "(SELECT id_musica FROM public.musicas_de_artista WHERE id_artista = " +
+                                "(SELECT id_artista FROM public.artista WHERE artist_name = ?))";
 
-                    "SELECT music_name FROM public.musica WHERE music_name = ?\n" +
-                    "UNION\n" +
+                PreparedStatement pstmt = connection.prepareStatement(ALLSQL);
+                pstmt.setString(1, word);
+                pstmt.setString(2, word);
+                pstmt.setString(3, word);
+                pstmt.setString(4, word);
+                ResultSet rs = pstmt.executeQuery();
 
-                    "SELECT music_name FROM public.musica WHERE genre = ?\n" +
-                    "UNION\n" +
+                //TRATAR DA INFORMAÇÃO RECEBIDA NO RESULT SET
+            }
+            else if (search_type.equals("music")){
+                String MUSICSQL = "SELECT music_name FROM public.musica WHERE music_name = ?";
+                PreparedStatement pstmt = connection.prepareStatement(MUSICSQL);
+                pstmt.setString(1, word);
+                ResultSet rs = pstmt.executeQuery();
 
-                    "SELECT music_name FROM public.musica WHERE id_musica = " +
+                //TRATAR DA INFORMAÇÃO RECEBIDA NO RESULT SET
+            }
+            else if (search_type.equals("artist")){
+                String ARTISTSQL = "SELECT music_name FROM public.musica WHERE id_musica = " +
                         "(SELECT id_musica FROM public.musicas_de_artista WHERE id_artista = " +
-                            "(SELECT id_artista FROM public.artista WHERE artist_name = ?))";
+                        "(SELECT id_artista FROM public.artista WHERE artist_name = ?))";
+                PreparedStatement pstmt = connection.prepareStatement(ARTISTSQL);
+                pstmt.setString(1, word);
+                ResultSet rs = pstmt.executeQuery();
 
-            PreparedStatement pstmt = connection.prepareStatement(ALLSQL);
-            pstmt.setString(1, word);
-            pstmt.setString(2, word);
-            pstmt.setString(3, word);
-            pstmt.setString(4, word);
-            ResultSet rs = pstmt.executeQuery();
+                //TRATAR DA INFORMAÇÃO RECEBIDA NO RESULT SET
+            }
+            else if (search_type.equals("album")){
+                String ALBUMSQL = "SELECT music_name FROM public.musica WHERE id_musica = " +
+                                    "(SELECT id_musica FROM public.musicas_de_album WHERE id_album = " +
+                                        "(SELECT id_album FROM public.album WHERE album_name = ?))";
+                PreparedStatement pstmt = connection.prepareStatement(ALBUMSQL);
+                pstmt.setString(1, word);
+                ResultSet rs = pstmt.executeQuery();
 
-            //TRATAR DA INFORMAÇÃO RECEBIDA NO RESULT SET
-        }
-        else if (search_type.equals("music")){
-            String MUSICSQL = "SELECT music_name FROM public.musica WHERE music_name = ?";
-            PreparedStatement pstmt = connection.prepareStatement(MUSICSQL);
-            pstmt.setString(1, word);
-            ResultSet rs = pstmt.executeQuery();
+                //TRATAR DA INFORMAÇÃO RECEBIDA NO RESULT SET
+            }
+            else if (search_type.equals("genre")){
+                String GENRESQL = "SELECT music_name FROM public.musica WHERE genre = ?";
+                PreparedStatement pstmt = connection.prepareStatement(GENRESQL);
+                pstmt.setString(1, word);
+                ResultSet rs = pstmt.executeQuery();
 
-            //TRATAR DA INFORMAÇÃO RECEBIDA NO RESULT SET
-        }
-        else if (search_type.equals("artist")){
-            String ARTISTSQL = "SELECT music_name FROM public.musica WHERE id_musica = " +
-                    "(SELECT id_musica FROM public.musicas_de_artista WHERE id_artista = " +
-                    "(SELECT id_artista FROM public.artista WHERE artist_name = ?))";
-            PreparedStatement pstmt = connection.prepareStatement(ARTISTSQL);
-            pstmt.setString(1, word);
-            ResultSet rs = pstmt.executeQuery();
-
-            //TRATAR DA INFORMAÇÃO RECEBIDA NO RESULT SET
-        }
-        else if (search_type.equals("album")){
-            String ALBUMSQL = "SELECT music_name FROM public.musica WHERE id_musica = " +
-                                "(SELECT id_musica FROM public.musicas_de_album WHERE id_album = " +
-                                    "(SELECT id_album FROM public.album WHERE album_name = ?))";
-            PreparedStatement pstmt = connection.prepareStatement(ALBUMSQL);
-            pstmt.setString(1, word);
-            ResultSet rs = pstmt.executeQuery();
-
-            //TRATAR DA INFORMAÇÃO RECEBIDA NO RESULT SET
-        }
-        else if (search_type.equals("genre")){
-            String GENRESQL = "SELECT music_name FROM public.musica WHERE genre = ?";
-            PreparedStatement pstmt = connection.prepareStatement(GENRESQL);
-            pstmt.setString(1, word);
-            ResultSet rs = pstmt.executeQuery();
-
-            //TRATAR DA INFORMAÇÃO RECEBIDA NO RESULT SET
-        }
+                //TRATAR DA INFORMAÇÃO RECEBIDA NO RESULT SET
+            }
 
 
+    }
 }
-
 
 
 public class Main {
