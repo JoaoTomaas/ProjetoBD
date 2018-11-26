8bacc5b1a18beb471222eedb486c68ba31775313
JoaoTomaas <revealconcepts@gmail.com>
2018-11-26 01:00:24 +0000

    Update CodigoProjeto.java

--- CodigoProjeto.java
+++ CodigoProjeto.java
@@ -2,6 +2,7 @@ import com.sun.corba.se.spi.orbutil.fsm.Guard;
 
 import javax.swing.plaf.metal.MetalToolBarUI;
 import java.sql.*;
+import java.util.zip.CheckedInputStream;
 
 class Metodos {
 
@@ -45,24 +46,33 @@ class Metodos {
 
             if (r.next()) {
                 if (Integer.parseInt(r.getString(1)) == 0) {
-                    String SQLRegAdmin = "insert into utilizador values (username, password, ?)";
-                    PreparedStatement p = connection.prepareStatement(SQLRegAdmin);
-                    p.setString(1, "admin");
-                    ResultSet rst = p.executeQuery();
+                    String SQLRegAdmin = "insert into utilizador values (?, ?, ?)";
+                    PreparedStatement pp = connection.prepareStatement(SQLRegAdmin);
+                    pp.setString(1, user);
+                    pp.setString(2, pass);
+                    pp.setString(3, "admin");
+                    pp.executeUpdate();
                     System.out.println("Utilizador registado como admin, welcome to DropMusic");
+                    pp.close();
                 } else {
-                    String SQLRegNormal = "insert into utilizador values (username, password, ?)";
+                    String SQLRegNormal = "insert into utilizador values (?, ?, ?)";
                     PreparedStatement p = connection.prepareStatement(SQLRegNormal);
-                    p.setString(1, "normal");
-                    ResultSet rst = p.executeQuery();
+                    p.setString(1, user);
+                    p.setString(2, pass);
+                    p.setString(3, "normal");
+                    p.executeUpdate();
                     System.out.println("Utilizador registado como normal, welcome to DropMusic");
+                    p.close();
                 }
             }
 
+            ps.close();
+            r.close();
             //Verificar se e necessario inicializar a null o ResultSet
         }
 
-
+        rs.close();
+        pstmt.close();
     }
 
     void executa_search_music(String search_type, String word, Connection connection) throws SQLException {
@@ -307,34 +317,32 @@ class Metodos {
 
     }
 
-    void executa_album_review(String user, String album_title, String rating, String text, Connection connection) throws SQLException {
+    void executa_album_review(String user, String album_title, String pontuacao, String text, Connection connection) throws SQLException {
         String AlbumReviewSQL = "INSERT INTO critica\n" +
                 "VALUES (DEFAULT, null, (SELECT id_album FROM album WHERE album_name = ?), ?, ?, ?)";
         PreparedStatement ps = connection.prepareStatement(AlbumReviewSQL);
         ps.setString(1, album_title);
         ps.setString(2, user);
         ps.setString(3, text);
-        ps.setString(4, rating);
-        ResultSet r = ps.executeQuery();
+        ps.setInt(4, Integer.parseInt(pontuacao));
+        ps.executeUpdate();
 
         System.out.println("Review do album submetida com sucesso");
         ps.close();
-        r.close();
     }
 
-    void executa_music_review(String user, String music_title, String rating, String text, Connection connection) throws SQLException {
+    void executa_music_review(String user, String music_title, String pontuacao, String text, Connection connection) throws SQLException {
         String MusicReviewSQL = "INSERT INTO critica " +
-                "VALUES (DEFAULT,  (SELECT id_musica FROM musica WHERE music_name = ?), null, ?, ?, ?)";
+                                "VALUES (DEFAULT,  (SELECT id_musica FROM musica WHERE music_name = ?), NULL , ?, ?, ?)";
         PreparedStatement p = connection.prepareStatement(MusicReviewSQL);
         p.setString(1, music_title);
         p.setString(2, user);
         p.setString(3, text);
-        p.setString(4, rating);
-        ResultSet rs = p.executeQuery();
+        p.setInt(4, Integer.parseInt(pontuacao));
+        p.executeUpdate();
 
         System.out.println("Review da musica submetida com sucesso");
         p.close();
-        rs.close();
     }
 
     void executa_make_editor(String user, String novo_user, Connection connection) throws SQLException {
@@ -345,21 +353,47 @@ class Metodos {
         ResultSet r = p.executeQuery();
 
         //SE TEM PERMISSAO -> CONDICAO 1
-        if (r.getString(1).equals("editor") || r.getString(1).equals("admin")) {
-            System.out.println("Pode tornar editor");
+        while (r.next()) {
+            if (r.getString(1).equals("editor") || r.getString(1).equals("admin")) {
+                System.out.println("Pode tornar editor");
 
-            //Fazer verificacao intermedia para ver se a pessoa ja e editora ou nao //TO DOOOOOOOOOOOOOO
+                //Verificar se o novo_user existe na base de dados
+                String NovoUserExisteSQL = "SELECT username FROM utilizador WHERE username = ?";
+                PreparedStatement pstt = connection.prepareStatement(NovoUserExisteSQL);
+                pstt.setString(1, novo_user);
+                ResultSet rrst = pstt.executeQuery();
 
-            String MakeEditorSQL = "UPDATE utilizador SET user_type = 'editor' WHERE username = ?";
-            PreparedStatement ps = connection.prepareStatement(MakeEditorSQL);
-            p.setString(1, novo_user);
-            ResultSet rs = ps.executeQuery();
+                if (rrst.next()) {
+                    System.out.println("Utilizador existe");
+
+                    //Fazer verificacao intermedia para ver se a pessoa ja e editora ou admin //TO DOOOOOOOOOOOOOO
+                    String CheckIfEditSQL = "SELECT user_type FROM utilizador WHERE username = ?";
+                    PreparedStatement pst = connection.prepareStatement(CheckIfEditSQL);
+                    pst.setString(1, novo_user);
+                    ResultSet rs = pst.executeQuery();
+
+                    while (rs.next()) {
+                        if (rs.getString(1).equals("normal")) {
+                            String MakeEditorSQL = "UPDATE utilizador SET user_type = 'editor' WHERE username = ?";
+                            PreparedStatement ps = connection.prepareStatement(MakeEditorSQL);
+                            ps.setString(1, novo_user);
+                            ps.executeUpdate();
+                        } else {
+                            System.out.println("Esse mano ja tem permissoes de editor");
+                        }
+                    }
+                }
+                else {
+                    System.out.println("Esse mano nao existe na nossa base de dados");
+                }
+            }
+         //SE NAO TEM PERMISSAO -> CONDICAO 2
+            else {
+                System.out.println("Nao tem autorizacao");
+                }
+
+
         }
-        //SE NAO TEM PERMISSAO -> CONDICAO 2
-        else {
-            System.out.println("Nao tem autorizacao");
-        }
-
     }
 
     void executa_cria_playlist(String user, String playlist_name, String privacy_type, Connection connection) throws SQLException {
@@ -368,34 +402,140 @@ class Metodos {
         PreparedStatement p = connection.prepareStatement(CriaPlaySQL);
         p.setString(1, playlist_name);
         p.setString(2, privacy_type);
-        ResultSet r = p.executeQuery();
+        p.executeUpdate();
 
-        //Associa a playlist ao utilizador que a criou (Visto que este pode ter várias
+        //Associa a playlist ao utilizador que a criou (Visto que este pode ter várias)
         String AssociaPlaySQL = "INSERT INTO cria_playlist VALUES (?, (SELECT id_playlist FROM playlist WHERE playlist_name = ?))";
         PreparedStatement ps = connection.prepareStatement(AssociaPlaySQL);
         ps.setString(1, user);
         ps.setString(2, playlist_name);
-        ResultSet rs = ps.executeQuery();
+        ps.executeUpdate();
 
-        System.out.println("Playlist " + playlist_name + "do user " + user + " criada com sucesso");
+        System.out.println("Playlist " + playlist_name + " do user " + user + " criada com sucesso");
     }
 
-    void executa_addmusic_playlist(String user, String playlist_name, Connection connection) throws SQLException {
-        //EM FALTA
+    void executa_opmusic_playlist(String user, String playlist_name, String op_type, String edit_type, String inp, Connection connection) throws SQLException {
+
+        String CheckaPrivacySQL = "SELECT privacy_type FROM playlist WHERE playlist_name = ?";
+        PreparedStatement p = connection.prepareStatement(CheckaPrivacySQL);
+        p.setString(1, playlist_name);
+        ResultSet r = p.executeQuery();
+
+        //1. Se for private, só o owner pode ver e alterar
+        if (r.getString(1).equals("private")){
+            //vai checkar se esse user é o dono
+            String checkaOwnerSQL = "SELECT username FROM cria_playlist WHERE id_playlist = " +
+                    "(SELECT id_playlist FROM playlist WHERE playlist_name = ?)";
+            PreparedStatement ps = connection.prepareStatement(checkaOwnerSQL);
+            ps.setString(1, playlist_name);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.getString(1).equals(user)){
+                System.out.println("Pode adicionar musicas a playlist");
+                //Vai adicionar musicas aqui
+            }
+            else{
+                System.out.println("Grupo privado");
+            }
+        }
+
+        //2. Se for publica, qualquer user pode ver
+        else if (r.getString(1).equals("public")){
+            System.out.println("ESTOU SO A BERE");
+        }
+
+        //3. Se for collaborative, qualquer user pode ver e editar
+        else if (r.getString(1).equals("collaborative")){
+            //Vai adicionar musicas aqui
+        }
+    }
+
+    void executa_ver_playlist(String user, String playlist_name, Connection connection) throws SQLException {
+        //1. Ver qual e o tipo de privacidade da playlist
+        String CheckaPrivacySQL = "SELECT privacy_type FROM playlist WHERE playlist_name = ?";
+        PreparedStatement p = connection.prepareStatement(CheckaPrivacySQL);
+        p.setString(1, playlist_name);
+        ResultSet r = p.executeQuery();
+
+        //1. Se for private, só o owner pode ver
+        while (r.next()) {
+            if (r.getString(1).equals("private")) {
+                String checkaOwnerSQL = "SELECT username FROM cria_playlist WHERE id_playlist = " +
+                        "(SELECT id_playlist FROM playlist WHERE playlist_name = ?)";
+                PreparedStatement ps = connection.prepareStatement(checkaOwnerSQL);
+                ps.setString(1, playlist_name);
+                ResultSet rs = ps.executeQuery();
+
+                while (rs.next()) {
+                    if (rs.getString(1).equals(user)) {
+                        System.out.println("Pode ver a playlist");
+                        //QUERY PARA VER AS MUSICAS DA PLAYLIST //EM FALTA
+                        String MusicasPlaySQL = "SELECT music_name, artist_name, album_name, genre, duration, filename FROM musica WHERE id_musica = " +
+                                "(SELECT id_musica FROM adiciona_musica WHERE id_playlist = " +
+                                "(SELECT id_playlist FROM playlist WHERE playlist_name = ?))";
+                        PreparedStatement pst = connection.prepareStatement(MusicasPlaySQL);
+                        pst.setString(1, playlist_name);
+                        ResultSet rst = pst.executeQuery();
+
+                        while (rst.next()) {
+                            System.out.println("Nome da musica: " + rst.getString(1));
+                            System.out.println("Nome do artista: " + rst.getString(2));
+                            System.out.println("Nome do album: " + rst.getString(3));
+                            System.out.println("Genero msuical: " + rst.getString(4));
+                            System.out.println("Duracao da musica: " + rst.getString(5));
+                            System.out.println("Nome do ficheiro: " + rst.getString(6));
+                        }
+
+                    } else {
+                        System.out.println("Grupo privado");
+                    }
+                }
+            } else if ((r.getString(1).equals("public")) || (r.getString(1).equals("collaborative"))) {
+                //QUERY PARA VER AS MUSICAS DA PLAYLIST
+                String MusicasPlaySQL = "SELECT music_name, artist_name, album_name, genre, duration, filename FROM musica WHERE id_musica = " +
+                        "(SELECT id_musica FROM adiciona_musica WHERE id_playlist = " +
+                        "(SELECT id_playlist FROM playlist WHERE playlist_name = ?))";
+                PreparedStatement pstt = connection.prepareStatement(MusicasPlaySQL);
+                pstt.setString(1, playlist_name);
+                ResultSet rstt = pstt.executeQuery();
+                System.out.println("Podes ver mano");
+
+                while (rstt.next()) {
+                    System.out.println("Nome da musica: " + rstt.getString(1));
+                    System.out.println("Nome do artista: " + rstt.getString(2));
+                    System.out.println("Nome do album: " + rstt.getString(3));
+                    System.out.println("Genero msuical: " + rstt.getString(4));
+                    System.out.println("Duracao da musica: " + rstt.getString(5));
+                    System.out.println("Nome do ficheiro:  " + rstt.getString(6));
+                }
+
+            }
+        }
     }
 
     void executa_cria_concerto(String user, String local, String data, String lotacao, Connection connection) throws SQLException {
-        String CriaConcertoSQL = "INSERT INTO concerto VALUES (DEFAULT, ?, ?, ?)";
+        String CriaConcertoSQL = "INSERT INTO concerto VALUES (DEFAULT, ?, to_date(?, 'DD/MM/YYYY'), ?)";
         PreparedStatement p = connection.prepareStatement(CriaConcertoSQL);
         p.setString(1, local);
         p.setString(2, data); //TESTAR (DEVE DAR ERRO)
-        p.setString(3, lotacao); //TESTAR (DEVE DAR ERRO)
-        ResultSet r = p.executeQuery();
-
+        p.setInt(3, Integer.parseInt(lotacao));
+        p.executeUpdate();
         System.out.println("Concerto criado");
+        p.close();
     }
 
-    //void executa_addartist_concerto (String user, String ){}
+    void executa_addartist_concerto (String user, String artist_name, String local, String data, Connection connection) throws SQLException {
+        String AddArtistSQL = "INSERT INTO dar_um_concerto\n" +
+                                 "VALUES ((SELECT id_artista FROM artista WHERE artist_name = ?), (SELECT id_concerto FROM concerto WHERE local = ? AND data = ?))";
+        PreparedStatement p = connection.prepareStatement(AddArtistSQL);
+        p.setString(1,artist_name);
+        p.setString(2,local);
+        p.setString(3, data);
+        p.executeUpdate();
+
+        System.out.println("Artista adicionado");
+        p.close();
+    }
 
     //void executa_alinhaconcerto
 
@@ -415,17 +555,27 @@ public class Main {
             //stmt = connection.createStatement();
             //rs = stmt.executeQuery("select username, password from utilizador where username = 'joao'");
             // Tanto posso escrever public.utilizador como utilizador
-            //while (rs.next())
 
             Metodos m = new Metodos();
-            m.executa_login("joao", "drogas", connection); //Esta a funcionar
-            //m.executa_registo(); //Esta a funcionar, falta testar
-            //m.executa_search_music("all", "kaleo", connection); //Esta a funcionar, falta testar
-            //m.executa_search_info("album", "views", connection); //TESTAR
-            //m.executa_album_review("joao", "views", "9", "ja ouvi pior e a pagar", connection); //TESTAR
-            //m.executa_music_review("sa", "Summertime Magic", "10", "quanto mais prima, mais se lhe arrima", connection); //TESTAR
-            //m.executa_make_editor("joao", "sa", connection); //TESTAR
-            //m.executa_cria_playlist("joao", "daily boost", "private", connection); //TESTAR
+            //m.executa_login("toni", "fifa", connection); //CHECK
+            //m.executa_registo("toni", "fifa", connection); //CHECK
+            //m.executa_make_editor("joao", "toni", connection); //CHECK
+            //m.executa_music_review("toni", "Summertime Magic", "9", "ja ouvi pior e a pagar", connection); //CHECK
+            //m.executa_album_review("toni", "Views", "8", "epa nao sei nao", connection); //CHECK
+            //m.executa_cria_playlist("toni", "funk do bom", "public", connection); //CHECK
+            //m.executa_ver_playlist("joao", "funk do bom", connection); //CHECK //VER DE NOVO QUANDO TIVER MÚSICAS NA PLAYLIST
+            //m.executa_cria_concerto("joao", "Passeio Maritimo de Alges", "11/07/2017", "100", connection); //CHECK
+
+
+            //m.executa_search_music("all", "Cenas", connection); //Esta a funcionar, falta testar a maior parte //music funciona
+            //m.executa_search_info("album", "Views", connection); //Esta a funcionar, falta testar a maior parte //music funciona
+
+            //FALTA TESTAR
+            //m.executa_addartist_concerto
+
+            //FALTA FAZER
+            //m.executa_opmusic_playlist
+            //m.executaalinhaconcerto
 
         } catch (SQLException e) {
             e.printStackTrace();
